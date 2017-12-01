eeff8e94b0baa5e094d894ec48cfb7d887e82055
Lucas Fogliarini <lucasfogliarini@gmail.com>
2017-12-01 21:33:08 -0200

    adjust view

--- app/services/marvelService.js
+++ app/services/marvelService.js
@@ -1,34 +1,39 @@
 angular.module('marvelStory')
-  .service('marvelService', ['$http', function($http){
+  .service('marvelService', ['$http', '$q', function($http, $q){
       this.getStory = function(storyId){
-          let story = this.httpGet('stories/'+storyId);
-          let characters = [];
-
-          return {
-             title: story.title,
-             description: story.description,
-             characters: characters
-          };
+          let def = $q.defer();
+          this.httpGet('stories/'+storyId).then(function (response) {
+              let story = response.data.data.results[0];
+              def.resolve({
+                 title: story.title,
+                 description: story.description
+              });
+          }, function (response) {
+              def.reject(response);
+          });
+          return def.promise;
       };
       this.getStoryCharacters = function(storyId){
-          return [{
-             name: "joao",
-             thumbnail: "b"
-          },
-          {
-             name: "d",
-             thumbnail: "a"
-          }];
+          let def = $q.defer();
+          this.httpGet('stories/'+storyId+'/characters').then(function (response) {
+              let characters = [];
+              response.data.data.results.forEach(function(char){
+                characters.push({
+                   id: char.id,
+                   name: char.name,
+                   //https://developer.marvel.com/documentation/images
+                   thumbnail: char.thumbnail.path + '/portrait_xlarge.' + char.thumbnail.extension
+                });
+              });
+              def.resolve(characters);
+          });
+          return def.promise;
       };
       //https://gateway.marvel.com/v1/public/{resourcePath}
       this.httpGet = function(resourcePath){
           let url = this.apiUrl + resourcePath;
           url += this.getKeyParams();
-          $http.get(url).then(function (response) {
-              return response;
-          }, function (response) {
-              throw new Error(response);
-          });
+          return $http.get(url);
       }
       this.apiUrl = 'https://gateway.marvel.com/v1/public/';
       this.getKeyParams = function()
--- app/story/story.js
+++ app/story/story.js
@@ -1,7 +1,12 @@
 function StoryController($scope, marvelService, $routeParams){
     (function init() {
         var storyId = $routeParams.id;
-        $scope.story = marvelService.getStory(storyId);
+        marvelService.getStory(storyId).then(function (story) {
+          $scope.story = story;
+        });
+        marvelService.getStoryCharacters(storyId).then(function (storyCharacters) {
+          $scope.storyCharacters = storyCharacters;
+        });
     })();
 }
 
